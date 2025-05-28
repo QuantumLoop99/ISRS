68dc038a49c7324ed4dd85c82da9beca1571f3f0
Isuranga Dasun Jayassri <166277109+Isuranga-2002@users.noreply.github.com>
2025-05-28 22:31:59 +0530

    Revert "Isuranga dasun"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
-export const routes: Routes = [
-    {path: 'signup', component: SignupComponent, title: 'Sign Up'},
-    {path: 'signin', component: SigninComponent, title: 'Sign In'},
-    { path: '', redirectTo: '/signup', pathMatch: 'full' }
-];
+export const routes: Routes = [];
