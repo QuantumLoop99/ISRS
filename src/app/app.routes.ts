e31bf9e50234cdcfa51bb00f62f3b5031ed533a0
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-05-28 21:57:33 +0530

    test commit # 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ApplicationFormComponent } from './application-form/application-form.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'application', component: ApplicationFormComponent },
-    {path: 'signup', component: SignupComponent, title: 'Sign Up'},
-    {path: 'signin', component: SigninComponent, title: 'Sign In'},
     { path: '', redirectTo: '/home', pathMatch: 'full' } 
 
 ];
